cbcc1d247e4d66eafcf16b17e765f5435d0e6de3
nbellic10010 <18678691331@163.com>
2021-04-01 11:09:23 +0800

    commit

--- src/main/java/com/mockserver/controller/MockLiveController.java
+++ src/main/java/com/mockserver/controller/MockLiveController.java
@@ -13,7 +13,7 @@ public class MockLiveController {
     static List<String> liveMatchList = new ArrayList<>();
     static Map<String, String> Situations = new HashMap<>();
     static Map<String, String> Situations_running = new HashMap<>();
-    private int moves;
+    private int moves = 2;
 
     static {
         liveMatchList.add("{\n" +
@@ -44,7 +44,7 @@ public class MockLiveController {
                 "    \"Result\":" +
                 "        {" +
                 "            \"sgf\":\"(;GM[1]FF[4]CA[UTF-8]SO[弈客围棋]RU[cn]KM[7.5]HA[0]AP[弈客围棋]SZ[19]PB[AA]PW[BB];B[pd];W[dd];B[pq];W[dq];B[co];W[qc];B[qd];W[pc];B[od];W[nb];B[ep];W[eq];B[fp];W[gq];B[cf];W[ch];B[qn];W[dk];B[mc];W[cc];B[ei];W[di];B[ee];W[fd];B[eh];W[ej];B[gp];W[hq];B[hp];W[iq];B[ip];W[jq];B[cj];W[ck];B[fj];W[fk];B[gk];W[gj];B[fi];W[bj];B[fl];W[ek];B[hj];W[nc];B[nd];W[md];B[kd];W[ld];B[lc];W[ke];B[jd];W[je];B[id];W[kc];B[kb];W[lb];B[jc];W[mb];B[kc];W[gi];B[gh];W[hi];B[hh];W[ii];B[ih];W[ji];B[jh];W[ki];B[kh];W[li];B[hk];W[lh];B[lg];W[mg];B[mf];W[ng];B[nf];W[pj];B[oh];W[og];B[ph];W[ni];B[rd];W[rc];B[nh];W[mh];B[qg];W[jl];B[jp];W[ml];B[fq];W[fr];B[gr];W[dr];B[kq];W[kr];B[lr];W[jr];B[lq];W[fe];B[ff];W[gc];B[ge];W[ef];B[df];W[gf];B[eg];W[he];B[ie];W[if];B[jf];W[om];B[sc];W[sb];B[sd];W[qb];B[bd];W[bc];B[fs];W[er];B[ql];W[pl];B[bl];W[dm];B[cm];W[dn];B[dp];W[cn];B[bn];W[cl];B[bm];W[fm];B[gm];W[el];B[qk];W[gl];B[hm];W[oo];B[po];W[oq];B[op];W[np];B[pp];W[nq];B[on];W[nn];B[no];W[mo];B[mn];W[oo];B[ed];W[ec];B[no];W[lf];B[kg];W[oo];B[hr];W[ir];B[no];W[ne];B[nm];W[of];B[hf];W[ln];B[lo];W[mm];B[mp];W[qi];B[rh];W[ri];B[si];W[sj];B[sh];W[qh];B[rg];W[hg];B[gg];W[pg];B[rj];W[qf];B[rf];W[gd];B[hb];W[gb];B[ig];W[km];B[bq];W[hl];B[il];W[fl];B[im];W[ik];B[kn];W[lm];B[jm];W[kk];B[ga];W[fa];B[ha];W[fb];B[qj];W[pi];B[oc];W[ob];B[kf];W[me];B[jk];W[jj];B[fn];W[jn];B[ko];W[nl];B[cr];W[cq];B[br];W[qe];B[re];W[nn];B[pk];W[mo];B[oo];W[ok];B[ks];W[hs];B[pm];W[en];B[fo];W[bf];B[bi];W[ci];B[bk];W[aj];B[ak];W[dj];B[ai];W[cj];B[bg];W[be];B[ce];W[cd];B[af];W[ad];B[cg];W[hc];B[ib];W[cp];B[bp];W[ol];B[le];W[ke];B[hf];W[bh];B[do];W[ah];B[ag];W[gs];B[js];W[ka];B[ja];W[la])\"," +
-                "            \"result\":\"\"," +
+                "            \"result\":\"\",\n" +
                 "            \"status\":2," +
                 "            \"id\":66113," +
                 "            \"delete_flag\":0" +
@@ -55,7 +55,7 @@ public class MockLiveController {
                 "    \"Status\": 1200,\n" +
                 "    \"Result\": {\n" +
                 "        \"sgf\": \"(;CA[utf-8]AP[abacus]SZ[19]PB[晓晓]PW[天天];B[pd];W[pp];B[dd];W[cp];B[eq];W[dq];B[ep];W[cn];B[nq];W[iq];B[qn];W[pn];B[pm])\",\n" +
-                "        \"result\": \"\"," +
+                "        \"result\": \"\",\n" +
                 "        \"status\": 2,\n" +
                 "        \"id\": 66199,\n" +
                 "        \"delete_flag\": 0\n" +
@@ -66,7 +66,7 @@ public class MockLiveController {
                 "    \"Status\": 1200,\n" +
                 "    \"Result\": {\n" +
                 "        \"sgf\": \"(;CA[utf-8]AP[abacus]SZ[19]PB[晓晓]PW[天天])\",\n" +
-                "        \"result\": \"\"," +
+                "        \"result\": \"\",\n" +
                 "        \"status\": 2,\n" +
                 "        \"id\": 66199,\n" +
                 "        \"delete_flag\": 0\n" +
@@ -78,7 +78,7 @@ public class MockLiveController {
                 "    \"Result\":" +
                 "        {" +
                 "            \"sgf\":\"(;GM[1]FF[4]CA[UTF-8]SO[弈客围棋]RU[cn]KM[7.5]HA[0]AP[弈客围棋]SZ[19]PB[AA]PW[BB])\"," +
-                "            \"result\":\"\"," +
+                "            \"result\":\"\",\n" +
                 "            \"status\":2," +
                 "            \"id\":66113," +
                 "            \"delete_flag\":0" +
@@ -105,20 +105,21 @@ public class MockLiveController {
         return Situations_running.get(liveId);
     }
 
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = 5000)
     public void fixSituation() {
         for(Map.Entry<String, String> entry: Situations.entrySet()) {
             String liveId = entry.getKey();
             String situation = entry.getValue();
             String[] sp = situation.split(",|:");
             String sgf = sp[4];
-            String[] st = sgf.split(";");
+            String[] st = sgf.split(";|\\)");
             if(moves < st.length) {
                 Situations_running.put(liveId, addMove(Situations_running.get(liveId), st[moves]));
             }
             int maxMoves = 70;
             if(moves == maxMoves) {
                 Situations_running.put(liveId, endLive(Situations_running.get(liveId)));
+
             }
         }
         moves++;
@@ -128,6 +129,8 @@ public class MockLiveController {
         String sgf = situation.split(",|:")[4];
         StringBuilder sb = new StringBuilder(sgf);
         sb.delete(sb.length() - 2, sb.length());
+        System.out.println("--------sgf---------");
+        System.out.println(sgf);
         sb.append(";").append(move).append("\")");
         return situation.replace(sgf, sb.toString());
     }
